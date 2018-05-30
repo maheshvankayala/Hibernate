b5b9507579b34eead4472e1ff81c9d5bd166b747
maheshvankayala <maheshvankayala@LAPTOP-CSR79S8H>
2018-05-30 15:23:58 -0400

    from test2

---
+++ Hibernate/src/main/java/com/mahesh/maven/App.java
@@ -0,0 +1,25 @@
+package com.mahesh.maven;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = session.beginTransaction();
+		Employee e1 = new Employee();
+		session.save(e1);
+		tx.commit();
+		session.close();
+		System.out.println("Added to test Git");
+	}
+
+}
